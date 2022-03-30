ac0689c804a50c26bfa0af715ae6ec0d59ae82e8
Cameron <cameronschrier>
2022-03-30 16:48:27 -0400

    First upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void test(View view) {
+        TextView textview = (TextView)findViewById(R.id.textView);
+        textview.setText("Test");
+    }
+
+    public void changview(View view) {
+        setContentView(R.layout.activity_second);
+    }
+}
