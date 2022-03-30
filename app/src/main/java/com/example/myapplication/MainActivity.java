564fadfa48218eb979ef02d90c777d2c0fdbb0fb
Cameron <cameronschrier>
2022-03-30 17:08:51 -0400

    First upload

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,11 +17,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void test(View view) {
-        TextView textview = (TextView)findViewById(R.id.textView);
-        textview.setText("Test");
+        //TextView textview = (TextView)findViewById(R.id.textView);
+        //textview.setText("Test");
     }
 
     public void changview(View view) {
         setContentView(R.layout.activity_second);
     }
+
+    public void warehouseView(View view) {
+        setContentView(R.layout.activity_warehouse_main);
+    }
+
 }
